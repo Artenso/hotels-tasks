7b7a3653630c069eb9510be7c885ee88fb037b6b
Konstantin <kostya.podsh@yandex.ru>
2024-04-25 21:40:06 +0300

    made task 2

--- task2/common_divisors.go
+++ task2/common_divisors.go
@@ -1,7 +1,36 @@
 package main
 
+import "math"
+
 func findDevidors(nums []int) []int {
-	devidor := 1
+
+	// if there is no values in nums
+	if len(nums) < 1 {
+		return []int{}
+	}
+
+	// if there is one value in nums
+	if len(nums) == 1 {
+		return getNumDevidors(nums[0])
+	}
+
+	// we get greatest common devidor for all nums
+	// and then get its devisors, so they will be
+	// common devisors
+	gcd := getGCD(nums[0], nums[1])
+	for i := 2; i < len(nums); i++ {
+		gcd = getGCD(gcd, nums[i])
+	}
+	return getNumDevidors(gcd)
+
+}
+
+// getNumDevidors gets all devisors of one num
+func getNumDevidors(num int) []int {
+
+	// create map to avoid duplicate values
+	devs := make(map[int]struct{})
+
 	// create results slice with len = 0 and
 	// cap = 4 because when we get more elements in nums,
 	// we will have fewer common divisors and
@@ -9,25 +38,34 @@ func findDevidors(nums []int) []int {
 	// however, this value allows us to avoid two allocations
 	// in cases where there are more than two divisors.
 	res := make([]int, 0, 4)
-	// while devidor < min num in nums
-	// increase devidor and check remainder of the division
-	// if remainder is not 0 we move to the next devidor
-	// else we check other nums and add devidor to results
-	for {
-		devidor++
-		isCommonDevidor := true
-		for _, num := range nums {
-			if devidor > num {
-				return res
-			}
-			if num%devidor != 0 {
-				isCommonDevidor = false
-				break
-			}
-		}
-		if isCommonDevidor {
-			res = append(res, devidor)
+
+	// we have to look for divisors up to the root of the num,
+	// because then there will be paired divisors and we get them like
+	// num/dev
+	for i := 1; i <= int(math.Sqrt(float64(num)))+1; i++ {
+		if num%i == 0 {
+			devs[i] = struct{}{}
+			devs[num/i] = struct{}{}
 		}
 	}
 
+	// delete 1, because there was no 1 in the example
+	delete(devs, 1)
+
+	// put devisors in slice
+	for dev, _ := range devs {
+		res = append(res, dev)
+	}
+	return res
+}
+
+// getGCD gets greatest common devidor
+// via Euclidean algorithm
+func getGCD(num1, num2 int) int {
+	for num2 != 0 {
+		temp := num2
+		num2 = num1 % num2
+		num1 = temp
+	}
+	return num1
 }
